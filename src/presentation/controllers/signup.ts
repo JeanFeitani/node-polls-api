80187a552c99c5cd04ef4cc7c91098f6cbe8a326
JeanFeitani <jeanls.folks@gmail.com>
2025-03-13 20:58:35 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,12 @@ import { BadRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      BadRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.name) {
-      BadRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        BadRequest(new MissingParamError(field))
+      }
     }
     return { statusCode: 500, body: new Error('Internal server error') }
   }
